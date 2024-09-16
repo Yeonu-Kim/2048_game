2eda5f5605d1d0ac29480517611370d5ef61879f
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-09-16 19:13:34 +0900

    [feat] Add game over function

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -1,20 +1,28 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import useGameBoard from './useGameBoard';
 import useMoveLeft from './useMoveLeft';
 import useRotateCells from './useRotateCells';
 
+export enum GameOverStatus {
+  Success = 'success',
+  Fail = 'fail',
+  None = 'none',
+}
+
 const useGame = () => {
   const {
     isMoved,
     cells,
+    getEmptyCellsIndex,
     setCells,
     setIsMoved,
     addTwoRandomCells,
-    addOneRandomCell,
   } = useGameBoard();
   const { rotateMapCounterClockwise } = useRotateCells();
   const { moveLeft } = useMoveLeft();
+
+  const [gameOver, setGameOver] = useState<GameOverStatus>(GameOverStatus.None);
 
   const moveCells = useCallback(
     (
@@ -29,12 +37,72 @@ const useGame = () => {
     [cells, moveLeft, rotateMapCounterClockwise, setCells, setIsMoved],
   );
 
+  const checkCanMove = useCallback(
+    (newCells: (number | null)[][]) => {
+      const rotateDirections: (0 | 90 | 180 | 270)[] = [0, 90, 180, 270];
+      return rotateDirections.some((rotateDirection) => {
+        const rotatedCells = rotateMapCounterClockwise(
+          newCells,
+          rotateDirection,
+        );
+        const moveResult = moveLeft(rotatedCells);
+
+        return moveResult.isMoved;
+      });
+    },
+    [moveLeft, rotateMapCounterClockwise],
+  );
+
+  const is128Exist = useCallback((newCells: (number | null)[][]) => {
+    return newCells.some((row) => row.includes(128));
+  }, []);
+
+  const checkNextTurn = useCallback(() => {
+    const emptyCells = getEmptyCellsIndex();
+
+    if (emptyCells.length === 0) return;
+
+    // 새로운 셀 생성
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    // 이 조건 깔끔하게 처리할 수 있는 방법 확인
+    if (emptyCells[randomIndex] !== undefined) {
+      const [rowIndex, colIndex] = emptyCells[randomIndex];
+
+      const newCells = cells.map((row) => [...row]);
+
+      if (newCells[rowIndex] !== undefined) {
+        newCells[rowIndex][colIndex] = 2;
+      }
+
+      // 다음 턴 진행 가능한지 확인
+      if (is128Exist(newCells)) {
+        setGameOver(GameOverStatus.Success);
+      }
+      if (!checkCanMove(newCells)) {
+        setGameOver(GameOverStatus.Fail);
+      }
+      setCells(newCells);
+    }
+
+    setIsMoved(false);
+  }, [
+    cells,
+    getEmptyCellsIndex,
+    checkCanMove,
+    is128Exist,
+    setCells,
+    setIsMoved,
+  ]);
+
   return {
     isMoved,
     cells,
+    gameOver,
     addTwoRandomCells,
-    addOneRandomCell,
+    checkNextTurn,
     moveCells,
+    checkCanMove,
+    is128Exist,
   };
 };
 
