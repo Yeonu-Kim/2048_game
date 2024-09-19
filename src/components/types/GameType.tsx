c55258dddf9e7694cdccf7e804335371b658841d
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-09-20 00:14:39 +0900

    [fix] Add types in all files

--- src/components/types/GameType.tsx
+++ src/components/types/GameType.tsx
@@ -1,9 +1,9 @@
-export type Cell = number | null;
-export type Cells = Cell[][];
-export type Direction = 'up' | 'left' | 'right' | 'down';
-export type RotateDegree = 0 | 90 | 180 | 270;
-export type DirectionDegreeMap = Record<Direction, RotateDegree>;
-export type MoveResult = { result: Cells; isMoved: boolean };
+// export type Cell = number | null;
+// export type Cells = Cell[][];
+// export type Direction = 'up' | 'left' | 'right' | 'down';
+// export type RotateDegree = 0 | 90 | 180 | 270;
+// export type DirectionDegreeMap = Record<Direction, RotateDegree>;
+// export type MoveResult = { result: Cells; isMoved: boolean };
 export enum GameOverStatus {
   Success = 'success',
   Fail = 'fail',
