4a55c6321f898244d30c3a2a66f16b5e6173eb01
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-09-24 20:41:19 +0900

    [fix] Remove unnecessary exports

--- src/usecases/scoreUtils.ts
+++ src/usecases/scoreUtils.ts
@@ -1,15 +1,3 @@
-import type { Cells } from '../entities/gameType.ts';
-
-export const getScore = (newCells: Cells) => {
-  const newScore = newCells.reduce(
-    (accRow, row) =>
-      accRow + row.reduce<number>((accCell, cell) => accCell + (cell ?? 0), 0),
-    0,
-  );
-
-  return newScore;
-};
-
 export const getHighScore = (newScore: number, prevHighScore: number) => {
   return newScore > prevHighScore ? newScore : prevHighScore;
 };
